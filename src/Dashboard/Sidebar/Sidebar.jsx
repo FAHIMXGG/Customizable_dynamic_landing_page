1b635f7de41c269bf7bb9979df6ec514f8b5babc
Shajib-Dv <mdsojeeb242@gmail.com>
2023-11-22 10:47:11 +0600

    header update

--- src/Dashboard/Sidebar/Sidebar.jsx
+++ src/Dashboard/Sidebar/Sidebar.jsx
@@ -5,23 +5,25 @@ import SidebarMenu from "./SidebarMenu";
 import MobileView from "./MobileView";
 
 const Sidebar = () => {
+  const headers = [];
+
+  for (let i = 1; i < 10; i++) {
+    headers.push(i);
+  }
+
   return (
     <div className="mt-10 flex gap-8">
       <SidebarMenu />
       <div className="flex-1">
         <div className="w-max flex items-center gap-6 h-fit lg:ml-7">
-          <button className="border-2 rounded px-2 text-gray-600">
-            Header 1
-          </button>
-          <button className="border-2 rounded px-2 text-gray-600">
-            Header 2
-          </button>
-          <button className="border-2 rounded px-2 text-gray-600">
-            Header 3
-          </button>
-          <button className="border-2 rounded px-2 text-gray-600">
-            Header 4
-          </button>
+          {headers.map((header) => (
+            <button
+              key={header}
+              className="border-2 rounded px-2 text-gray-600"
+            >
+              Header {header}
+            </button>
+          ))}
         </div>
         <div className="w-full h-full mt-8 flex flex-col lg:flex-row gap-12">
           <div className="w-full">
