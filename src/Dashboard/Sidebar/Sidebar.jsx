dc7d96205fa75196f1eb40164ff94eab37df4804
Shajib-Dv <mdsojeeb242@gmail.com>
2023-11-23 13:40:49 +0600

    stash reconnect

--- src/Dashboard/Sidebar/Sidebar.jsx
+++ src/Dashboard/Sidebar/Sidebar.jsx
@@ -6,18 +6,20 @@ import MobileView from "./MobileView";
 import { useState } from "react";
 import Cover from "../../components/dashboard/Cover/Cover";
 import HomeComponent from "../../components/dashboard/HomeComponent";
-import { IoArrowForwardCircleOutline, IoArrowBackCircleOutline } from "react-icons/io5";
+import {
+  IoArrowForwardCircleOutline,
+  IoArrowBackCircleOutline,
+} from "react-icons/io5";
 
 const Sidebar = () => {
-  const [hover, setHover] = useState()
-  const [click, setClick] = useState(1)
+  const [hover, setHover] = useState();
+  const [click, setClick] = useState(1);
   const [isHovered, setIsHovered] = useState(false);
-
 
   const decreaseClick = () => {
     setClick((prevClick) => (prevClick > 1 ? prevClick - 1 : prevClick));
   };
-  
+
   const increaseClick = () => {
     setClick((prevClick) => (prevClick < 9 ? prevClick + 1 : prevClick));
   };
@@ -31,25 +33,25 @@ const Sidebar = () => {
   };
 
   const hoverTextStyle = {
-    cursor: 'pointer',
-    position: 'relative',
+    cursor: "pointer",
+    position: "relative",
   };
 
   const hoverWindowStyle = {
-    display: 'block',
-    position: 'absolute',
-    top: '100%',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    backgroundColor: '#fff',
-    padding: '10px',
-    border: '1px solid #ccc',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    display: "block",
+    position: "absolute",
+    top: "100%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    backgroundColor: "#fff",
+    padding: "10px",
+    border: "1px solid #ccc",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     zIndex: 1,
-    width: '1400px',
-    height: '704px',
+    width: "1400px",
+    height: "704px",
     opacity: isHovered ? 1 : 0,
-    transition: 'opacity 0.3s ease-in-out',
+    transition: "opacity 0.3s ease-in-out",
   };
 
   const headers = [];
@@ -58,13 +60,13 @@ const Sidebar = () => {
   }
 
   const handleHover = (header) => {
-    setHover(header)
-  }
+    setHover(header);
+  };
   //console.log(hover)
 
   const handleClick = (header) => {
-    setClick(header)
-  }
+    setClick(header);
+  };
 
   //console.log(click)
 
@@ -73,50 +75,52 @@ const Sidebar = () => {
       <SidebarMenu />
       <div className="flex-1 ">
         <div className="flex items-center justify-center">
-          <div className="w-max flex items-center gap-6 h-fit lg:ml-7"
-            style={{ position: 'relative' }}>
+          <div
+            className="w-max flex items-center gap-6 h-fit lg:ml-7"
+            style={{ position: "relative" }}
+          >
             {headers.map((header) => (
               <button
                 key={header}
                 className="border-2 rounded px-2 text-gray-600"
-                onMouseOver={() => { handleHover(header) }}
-                onClick={() => { handleClick(header) }}
+                onMouseOver={() => {
+                  handleHover(header);
+                }}
+                onClick={() => {
+                  handleClick(header);
+                }}
                 style={hoverTextStyle}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
-
               >
                 Header {header}
               </button>
             ))}
-            <div
-              className="inline-style-tailwind"
-              style={hoverWindowStyle}
-            >
-
-              <div className='flex gap-3'>
-                <div className='py-20 w-[1000px]'>
+            <div className="inline-style-tailwind" style={hoverWindowStyle}>
+              <div className="flex gap-3">
+                <div className="py-20 w-[1000px]">
                   <Cover hover={hover} />
                 </div>
-                <div className='w-[380px] h-60'>
+                <div className="w-[380px] h-60">
                   <MobileView hover={hover} />
                 </div>
               </div>
-
             </div>
           </div>
         </div>
         <div className="flex gap-2">
-          <button onClick={decreaseClick}><IoArrowBackCircleOutline /></button>
+          <button onClick={decreaseClick}>
+            <IoArrowBackCircleOutline />
+          </button>
           <div className="w-full h-full mt-8 flex flex-col lg:flex-row gap-5">
             <div className="w-full">
               <HomeComponent click={click} />
             </div>
             <MobileView />
           </div>
-          <button
-            onClick={increaseClick}
-          ><IoArrowForwardCircleOutline /></button>
+          <button onClick={increaseClick}>
+            <IoArrowForwardCircleOutline />
+          </button>
         </div>
       </div>
     </div>
